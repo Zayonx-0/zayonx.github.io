6f43c5bdfe874e7c3ce127c26c228b621a6fe1dd
Zayonx-0 <matyas974yt@gmail.com>
2024-01-29 12:02:05 +0100

    add nodejs as skill

--- src/lib/params.ts
+++ src/lib/params.ts
@@ -14,7 +14,7 @@ import {
 } from './types';
 import { Icons } from './utils';
 
-export const TITLE_SUFFIX = 'Slick template with Svelte';
+export const TITLE_SUFFIX = 'Matyas Ricci - Développeur';
 
 export const NavBar = {
 	home: 'Home',
