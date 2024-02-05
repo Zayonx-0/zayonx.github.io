b3910df5f9019510293d12e94a8ffa3d1abdbc90
Zayonx-0 <matyas974yt@gmail.com>
2024-02-05 10:22:27 +0100

    Update dates

--- src/lib/educations.params.ts
+++ src/lib/educations.params.ts
@@ -9,7 +9,7 @@ export const MY_EDUCATIONS: Array<Education> = [
 		logo: Assets.Unknown,
 		name: '',
 		organization: 'IUT GEII Toulon',
-		period: { from: new Date(2020, 0, 1) },
+		period: { from: new Date(2021, 9, 1)n to: new Date(2024, 8, 30)},
 		shortDescription: '',
 		slug: 'dummy-education-item',
 		subjects: ['C', 'Algorithm', 'Algebra', 'Python', 'C++', 'Java', 'Anglais', 'Electronique', 'Electrotechnique', 'Maths']
@@ -21,7 +21,7 @@ export const MY_EDUCATIONS: Array<Education> = [
 		logo: Assets.Unknown,
 		name: '',
 		organization: 'Lycée du Coudon',
-		period: { from: new Date(2018), to: new Date(2021)},
+		period: { from: new Date(2018, 9, 1), to: new Date(2021, 7, 4)},
 		shortDescription: '',
 		slug: 'dummy-education-item-2',
 		subjects: ['Python', 'Architecture des Ordinateurs', 'Algorithmes et Structures des Données', 'C', 'Base de Données', 'Développement Web', 'Anglais']
