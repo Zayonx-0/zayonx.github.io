3cf58df2b69b49cf1eacf0c00c2eb227db13272f
Zayonx-0 <matyas974yt@gmail.com>
2024-08-29 01:24:44 +0200

    Finish portfolio ?

--- src/lib/projects.params.ts
+++ src/lib/projects.params.ts
@@ -7,7 +7,7 @@ const MY_PROJECTS: Array<Project> = [
 		slug: 'Projet "NetTest"',
 		color: '#5e95e3',
 		description:
-			'Projet réalisé en entreprise, consistant à la réalisation d\'un outil de test de débit internet. Conçut pour être utilisé par les techniciens de l\'entreprise. Réalisé avec Python ainsi que l\'outil de test "Iperf".',
+			'Projet réalisé en entreprise, consistant à la réalisation d\'un outil de test de débit internet. Conçut pour être utilisé par les techniciens de l\'entreprise. Réalisé avec Python ainsi que l\'outil de test "Iperf".\NCe script permet entre autre d\'automatiser ainsi que de rendre possible l\'auto-test d\'un équipement concernant ces interfaces réseau.',
 		shortDescription:
 			'Projet réalisé en entreprise, consistant à la réalisation d\'un outil de test de débit internet.',
 		links: [{ to: 'https://github.com/Zayonx-0/zayonx-0.github.io', label: 'GitHub' }],
@@ -21,16 +21,16 @@ const MY_PROJECTS: Array<Project> = [
 		type: 'NetTest',
 		screenshots: [
 			{
-				label: 'Programme en cours de fonctionnement',
-				src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwmIz-Xq-aFCktl4o-SzvaIjCHtffBeoNfUw&usqp=CAU'
+				label: 'Choix des interfaces à utiliser pour le test',
+				src: 'https://i.imgur.com/ZYiLt9Y.png'
+			},
+			{
+				label: 'Clignottement de la led associé à la sélection actuelle des interfaces',
+				src: 'https://i.imgur.com/eKZhSsa.png'
 			},
 			{
 				label: 'Questions posés à l\'utilisateur pour la configuration du test',
-				src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwmIz-Xq-aFCktl4o-SzvaIjCHtffBeoNfUw&usqp=CAU'
-			},
-			{
-				label: 'Fichier PDF généré par le programme, récapitulant les résultats du test',
-				src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwmIz-Xq-aFCktl4o-SzvaIjCHtffBeoNfUw&usqp=CAU'
+				src: 'https://i.imgur.com/t3RO5es.png'
 			}
 		]
 	},
@@ -51,36 +51,24 @@ const MY_PROJECTS: Array<Project> = [
 		type: 'Portfolio Web',
 		screenshots: [
 			{
-				label: 'Exemple de screen 1',
+				label: 'Emplacement d\'Image 1',
 				src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
 			},
 			{
-				label: 'Exemple de screen 2',
+				label: 'Emplacement d\'Image 2',
 				src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
 			},
 			{
-				label: 'Exemple de screen 3',
+				label: 'Emplacement d\'Image 3',
 				src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
 			},
-			{
-				label: 'Exemple de screen 4',
-				src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: 'Exemple de screen 5',
-				src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			},
-			{
-				label: 'Exemple de screen 6',
-				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
-			}
 		]
 	},
 	{
 		slug: 'Projet Decodage & Encodage Morse',
 		color: '#ff3e00',
 		description:
-			'Projet Python d\'Encodage/Decodage de code morse (audio <-> texte)',
+			'Projet Python d\'Encodage/Decodage de code morse (audio <-> texte)\nIl a été réalisé lors de ma dernière année de lycée dans la matière "NSI" (Numérique et Sciences Informatiques).',
 		shortDescription:
 			'Projet Python consistant au décodage de code morse contenu dans un fichier audio / Encodage depuis un texte vers un fichier audio.',
 		links: [{ to: 'https://github.com/Zayonx-0/Morse-Code', label: 'GitHub' }],
@@ -95,7 +83,7 @@ const MY_PROJECTS: Array<Project> = [
 		screenshots: [
 			{
 			label: 'Résultat de l\'encodage d\'un texte en morse',
-			src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwmIz-Xq-aFCktl4o-SzvaIjCHtffBeoNfUw&usqp=CAU'
+			src:'https://i.imgur.com/tPaJgJL.png'
 			}
 		]
 
@@ -104,7 +92,7 @@ const MY_PROJECTS: Array<Project> = [
 		slug: 'Projet "TradeBot"',
 		color: '#ff3e00',
 		description:
-			'Projet de développement d\'un bot de trading d\'objets virtuels sur le jeu Team Fortress 2.',
+			'Projet de développement d\'un bot de trading d\'objets virtuels sur le jeu Team Fortress 2.\n Le bot est capable de réaliser des échanges automatiques avec d\'autres utilisateurs du jeu avec des objets virtuels ayant une valeur monétaire.\nLe but de fonctionnement étais de réaliser des profits sur les échanges, en achetant des objets à bas prix pour les revendre plus cher.',
 		shortDescription:
 			'Projet de développement d\'un bot de trading d\'objets virtuels sur le jeu Team Fortress 2.',
 		links: [{ to: 'https://github.com/Zayonx-0/AUTOTRADE_TF2', label: 'GitHub' }],
@@ -118,14 +106,6 @@ const MY_PROJECTS: Array<Project> = [
 		type: 'Projet Personel',
 		screenshots: [
 			{
-			label: 'Logs de Debug du programme en cours de fonctionnement',
-			src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwmIz-Xq-aFCktl4o-SzvaIjCHtffBeoNfUw&usqp=CAU'
-			},
-			{
-			label: 'Logs de debug du programme en cours d\'actualisation des prix',
-			src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwmIz-Xq-aFCktl4o-SzvaIjCHtffBeoNfUw&usqp=CAU'
-			},
-			{
 			label: 'Interface Discord corresondant au récapitulatif des statistiques du bot',
 			src:'https://i.imgur.com/rEUsj8X.png'
 			}
@@ -135,7 +115,7 @@ const MY_PROJECTS: Array<Project> = [
 		slug: 'Projet "SQL Base de données Vol"',
 		color: '#ff3e00',
 		description:
-			'Projet base de données SQL concernant la gestion de vols aériens. (Recherche de vols, aéroports, compagnies aériennes, etc...) - Réalisé avec MampServer.',
+			'Projet base de données SQL concernant la gestion de vols aériens. (Recherche de vols, aéroports, compagnies aériennes, etc...) - Réalisé avec MampServer.\nProjet universitaire réalisé dans le cadre d\'un TP de base de données SQL.',
 		shortDescription:
 			'Projet base de données SQL concernant la gestion de vols aériens.',
 		links: [{ to: 'https://github.com/Zayonx-0/SQL_Projet_VOL', label: 'GitHub' }, { to: 'https://github.com/Zayonx-0/SQL_Projet_VOL/blob/main/R5.07%20Projet%20TP2%20Vols.pdf', label: 'Sujet' }],
@@ -170,7 +150,7 @@ const MY_PROJECTS: Array<Project> = [
 		slug: 'Projet Robot',
 		color: '#ff3e00',
 		description:
-			'Projet consistant en la réalisation d\'un robot détecteur d\'obstacles. Réalisé avec Arduino. Le code compris sur la page Github a permis de finir 1er au concours robot réalisé en 2021',
+			'Projet consistant en la réalisation d\'un robot détecteur d\'obstacles. Réalisé avec Arduino.\n Le but du projet étais de réaliser le robot le plus performant possible dans le cadre d\'un mini concours réalisé au sein de l\'université. Le code partagé sur la page Github a permis de finir 1er au concours robot réalisé en 2021',
 		shortDescription:
 			'Projet consistant en la réalisation d\'un robot détecteur d\'obstacles. Réalisé avec Arduino.',
 		links: [{ to: 'https://github.com/Zayonx-0/Projet_Robot', label: 'GitHub' }],
@@ -208,11 +188,11 @@ const MY_PROJECTS: Array<Project> = [
 		screenshots: [
 			{
 			label: 'Schéma de la carte électronique',
-			src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwmIz-Xq-aFCktl4o-SzvaIjCHtffBeoNfUw&usqp=CAU'
+			src:'https://i.imgur.com/wqNFiHH.png'
 			},
 			{
 			label: 'Carte électronique complète',
-			src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwmIz-Xq-aFCktl4o-SzvaIjCHtffBeoNfUw&usqp=CAU'
+			src:'https://i.imgur.com/VRjt9ep.jpeg'
 			}
 		]
 	},
